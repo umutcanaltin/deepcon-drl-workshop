d74c6859ee3665cf44476498f26f47e516edea3f
Umut Can ALTIN <umutcanaltin1@gmail.com>
2018-10-02 19:54:56 +0300

    add ipynb file

--- dqn_usage.py
+++ dqn_usage.py
@@ -7,9 +7,9 @@ action_size = env.action_space.n
 agent = DQN(state_size, action_size,epsilon = 0.9, epsilon_decrease=True, epsilon_min=0.2)
 done = False
 batch_size = 32
-max_time=250
-episodes=5
-scores=[]
+max_time = 250
+episodes = 5
+scores = []
 for e in range(episodes):
     state = env.reset()
     for time in range(max_time):
@@ -17,13 +17,13 @@ for e in range(episodes):
         action = agent.select_action(state)
         next_state, reward, done, _ = env.step(action)
         if done:
-            reward = -10   #if done we have to punish the agent with -10
+            reward = -10  # if done we have to punish the agent with -10
         agent.remember(state, action, reward, next_state, done)
         state = next_state
         if done:
             scores.append(time)
             print("episode: {}/{}, score: {}"
-                    .format(e+1, episodes, time))
+                  .format(e+1, episodes, time))
             break
         agent.learn_from_memory(batch_size)
 print(scores)
