694e99ca1f522b60c98c17007131c82ce6dfa53a
Umut Can ALTIN <umutcanaltin1@gmail.com>
2018-10-02 20:17:48 +0300

    add model save to usage file

--- dqn_keras.py
+++ dqn_keras.py
@@ -20,22 +20,22 @@ class policy():
 
 
 class DQN:
-    def __init__(self, state_size, action_size, epsilon=0.5, epsilon_min = 0.1, model_test = False, epsilon_decrease=False, epsilon_decay=0.95):
+    def __init__(self, state_size, action_size, epsilon=0.5, epsilon_min=0.1, model_test=False, epsilon_decrease=False, epsilon_decay=0.95):
 
         self.state_size = state_size
         self.action_size = action_size
         self.memory = deque(maxlen=2000)
         self.gamma = 0.95    # discount rate
-        self.epsilon = epsilon # exploration rate
+        self.epsilon = epsilon  # exploration rate
         self.learning_rate = 0.001
-        self.epsilon_decrease=epsilon_decrease
+        self.epsilon_decrease = epsilon_decrease
         self.epsilon_decay = epsilon_decay
         self.epsilon_min = epsilon_min
-        self.policy =policy(action_space=self.action_size)
+        self.policy = policy(action_space=self.action_size)
         if not model_test:
             self.model = Sequential()
             self._build_model()
-        else :
+        else:
             self.epsilon = 0
             print("For test purpose, the epsilon value was drawn to 0.")
 
@@ -44,8 +44,7 @@ class DQN:
         self.model.add(Dense(24, activation='relu'))
         self.model.add(Dense(self.action_size, activation='linear'))
         self.model.compile(loss=losses.mean_squared_error,
-                      optimizer=optimizers.Adam(lr=self.learning_rate))
-
+                           optimizer=optimizers.Adam(lr=self.learning_rate))
 
     def remember(self, state, action, reward, next_state, done):
         state = np.array([state])
@@ -53,7 +52,7 @@ class DQN:
         self.memory.append((state, action, reward, next_state, done))
 
     def select_action(self, state):
-        state = np.array([state]) #vectorize for prediction
+        state = np.array([state])  # vectorize for prediction
         q_values_for_actions = self.model.predict(state)
         a_prob = self.policy.probs(q_values_for_actions, self.epsilon)
 
@@ -70,19 +69,23 @@ class DQN:
 
                 predicted_q_values = self.model.predict(state)  # [[predicted_q_value_of_action_1,predicted_q_value_of_action_2]]
 
-                q_values_for_training= predicted_q_values
-                q_values_for_training[0][action]=target       # new action q values(for training)
+                q_values_for_training = predicted_q_values
+                q_values_for_training[0][action] = target       # new action q values(for training)
 
                 self.model.fit(state, q_values_for_training, epochs=1, verbose=0)  #trainig with new q values
             if self.epsilon > self.epsilon_min:
                 self.epsilon *= self.epsilon_decay
+
     def get_model(self):
         return self.model
-    def set_model(self,model):
-        self.model=model
-    def save_model(self,name):
+
+    def set_model(self, model):
+        self.model = model
+
+    def save_model(self, name):
         self.model.save_weights(name)
-    def load_model(self,name):
+
+    def load_model(self, name):
         self.model.load_weights(name)
 
 
